29ff39631ef78350541453bbbd802689b23b555c
menonna <35532054+menonna@users.noreply.github.com>
2021-04-23 23:11:38 +0200

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,7 +5,7 @@ import Header from "../components/testa"
 export default function About() {
   return (
     <div style={{ color: `teal` }}>
-      <Header />
+      <Header headerText="About Gatsby di marco" />
       <p>Such wow. Very Reactttt.</p>
     </div>
   )
